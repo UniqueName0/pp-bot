3f1c2d70c8b0c2277e371bb371a5605198645fb1
UniqueName2 <name37655@gmail.com>
2020-12-06 21:55:48 +0000

    fixed economy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import random
-
+import json
 
 token = os.getenv('token')
 
@@ -60,5 +60,54 @@ async def changerepeatlimit(ctx,arg):
 async def stop(ctx):
   global stopped
   stopped = 1
+  
+@bot.command()
+async def balance(ctx):
+  await open_account(ctx.author)
+  user = ctx.author
+  users = await get_bank_data()
+  
+  wallet_amt = users[str(user.id)]["wallet"]
+  
+  em = discord.Embed(title = f"{ctx.author.name}'s balance", color = discord.Color.red())
+  em.add_field(name = "pp points", value = wallet_amt)
+  await ctx.send(embed = em)
+  
+@bot.command()
+async def work(ctx):
+  await open_account(ctx.author)
+  
+  users = await get_bank_data()
+  user = ctx.author
+  earnings = random.randrange(101)
+  
+  await ctx.send(f"you got {earnings} pp points")
+  
+  users[str(user.id)]["wallet"] += earnings
+  
+  with open("mainbank.json", "w") as f:
+    json.dump(users, f)
+  
+  
+async def open_account(user):
+  users = await get_bank_data()
+  if str(user.id) in users:
+    return False
+  else:
+    users[str(user.id)] = {}
+    users[str(user.id)]["wallet"] = 0
+    
+  with open("mainbank.json", "w") as f:
+    json.dump(users,f)
+  return True
+  
+  
+async def get_bank_data():
+  with open("mainbank.json", "r") as f:
+    users = json.load(f)
+    
+  return users
+    
+  
   
 bot.run(token)
